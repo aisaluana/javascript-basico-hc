6fdffb9707730dc75a205e587001587ce4ed4270
Aisa Luana <aisalgmb@gmail.com>
2022-05-25 15:43:57 -0300

    Adiciona arquivo operadores.js

---
+++ 2.numeros-operadores/operadores.js
@@ -0,0 +1,20 @@
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+console.log(resultado);
+
+var nome = Aisa;
+var sobrenome = Luana;
+
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+var primeiroValor = 10;
+var segundoValor = '3'; //ainda funciona
+
+var resultado = primeiroValor - segundoValor;
+
+console.log(resultado2);
